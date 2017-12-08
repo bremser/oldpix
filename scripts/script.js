947ecdac71cd4691348f2cc951eb61adceb5bae1
bremser <wbremser@gmail.com>
2017-12-08 10:50:00 -0800

    small updates

--- scripts/script.js
+++ scripts/script.js
@@ -76,8 +76,13 @@ module.exports = function(robot) {
         return msg.http('http://www.loc.gov/pictures/search/?fo=json&fa=displayed:anywhere&fi=date&q=' + photoQuery).get()(function(err, res, body) {
           var image, images, response, photographer, date, photourl, title;
           response = JSON.parse(body);
-            if (response.search.hits > 2) {
-            if (photoQuery > 1944 || photoQuery < 1935) { photoQuery = (photoQuery + " (note: the depression years archive at LOC covers 1935-1944)") };
+          if (response.search.hits > 2) {
+            // return msg.http('http:www.loc.gov/pictures/search/?fo=json&fi=date&sp=2&fa=displayed%3Aanywhere&q=' + photoQuery).get()(function(err, res, body) {
+            //
+            //   responseTwo = JSON.parse(body);
+            //   pageTwo = responseTwo.results.length;
+            // };
+            if (photoQuery > 1944 || photoQuery < 1935) { photoQuery = (photoQuery + " (note: the depression years archive at LOC covers 1935-1944)")};
             let images = response.results;
             let rando = getRando(0, response.results.length);
             if (response.results[rando].title !== null) {
@@ -96,7 +101,7 @@ module.exports = function(robot) {
               photographer = response.results[rando].creator;
             } else {photographer = "(no credit)"};
             if (images.length > 0) {
-            return msg.send( "http:" + photourl + "\n find date: *" + photoQuery  + "* \n photo date: " + date + "   \n photo title: " + title + "\n by: " + fixName(photographer) + "\n more info at - http:" + response.results[rando].links.item + page2hits);
+            return msg.send( "http:" + photourl + "\n find date: *" + photoQuery  + "* \n photo date: " + date + "   \n photo title: " + title + "\n by: " + fixName(photographer) + "\n more info at - http:" + response.results[rando].links.item );
           }
         }  else { return msg.send("*" + photoQuery + "*" + " - no results, try again! (sample: oldpix date 1937)" );}
         });
