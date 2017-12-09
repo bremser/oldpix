9743ff63391038972a1030af8af28e94793debf4
bremser <wbremser@gmail.com>
2017-12-08 20:30:19 -0800

    small updates

--- scripts/script.js
+++ scripts/script.js
@@ -23,8 +23,11 @@ module.exports = function(robot) {
       var locURL = 'http://www.loc.gov/pictures/search/?fo=json&fa=displayed:anywhere&fi=date&q=';
         photoQuery = escape(msg.match[1]);
         return msg.http(locURL + photoQuery).get()(function(err, res, body) {
-          var image, images, photographer, response, date, photourl, title;
-          if (photoQuery > 1944 || photoQuery < 1935) { photoQuery = (photoQuery + "\n (note: the depression years archive at LOC covers 1935-1944)")};
+          var image, images, photographer, response, date, fsaSuggest, photourl, title;
+          if (photoQuery > 1944 || photoQuery < 1935) {
+            let fsaSuggest = "\n (note: the depression years archive at LOC covers 1935-1944)")
+          } else { let fsaSuggest = ''
+          };
           response = JSON.parse(body);
           if (response.search.hits > 2) {
               // return msg.http('http://www.loc.gov/pictures/search/?fo=json&fa=displayed:anywhere&sp=2fi=date&q=' + photoQuery).get()(function(err, res, body) {
@@ -51,9 +54,9 @@ module.exports = function(robot) {
               photographer = response.results[rando].creator;
             } else {photographer = "no photographer credit"};
             if (images.length > 0) {
-            return msg.send( "http:" + photourl + "\n find date: *" + photoQuery  + "* \n photo date: " + date + "   \n photo title: " + title + "\n by: " + fixName(photographer) + "\n more info at - http:" + response.results[rando].links.item);
+            return msg.send( "http:" + photourl + "\n find date: *" + photoQuery + fsaSuggest + "* \n photo date: " + date + "   \n photo title: " + title + "\n by: " + fixName(photographer) + "\n more info at - http:" + response.results[rando].links.item);
           }
-          }  else { return msg.send( photoQuery  + "didn't turn anything up for some reason" );}
+        }  else { return msg.send( "\n  not much turned up for:" photoQuery  +  );}
         });
       });
 
