d88ee5678f69e4d96f113ac92a9a334a75554b3a
bremser <wbremser@gmail.com>
2017-12-15 15:12:16 -0800

    added random second page of results to take random result from

--- README.md
+++ README.md
@@ -1,23 +1,22 @@
 # oldpix
 
-oldpix is a chat bot built on the Hubot framework, designed to be used in Slack.
-
+Oldpix is a chat bot built with Hubot that gets random Library of Congress photos, based on several commands. It's designed to be used in Slack with that unfurls the resulting photos in your channel or DMs with the bot.
 
 ### Configuration
 
-None, it's very basic.
+It's very basic, but if you want to use it with Slack you'll need to get a Slack API token for your slack, and if you host oldpix on Heroku put the token in the Config Vars (as HUBOT_SLACK_TOKEN) in the Settings for your app.
 
 ### Commands
 
-Oldpix gets random Library of Congress photos, based on several commands.
+ Designed for using with Slack.
 
 **oldpix find airplane**
 
-will result in photo based on query term of title/subjects fields
+Find results in photo based on query term of title/subjects fields
 
 **oldpix date 1936**
 
-will result in a photo based on a query for the date field
+Date results in a photo based on a query for the date field. Due to inconsistencies with how LoC uses the date field, sometimes the results reflect the start/end of a date range.
 
 **oldpix famous**
 
@@ -28,6 +27,6 @@ will result in one of several famous photographers (eg. Dorothea Lange) being se
 * oldpix date 1937
 * oldpix famous
 
-### Issues
+### Considerations
 
-The LoC returns 25 results for each page of a query, next version will get more results to make it even more random when you search for the same term/date repeatedly.
+The LoC returns 20 results for each page of a query, the bot currently gets the first page of results, and a random second page (eg. page 5 with, results 100-120) and then chooses a random result from that group of 40. This is in an effort to make the results feel both relevant and random, especially if you repeatedly hit the same query. Related issue is photographs from the same roll of film or session are grouped together in the archive, which can fill up 10 or more slots in a page of results.  
--- scripts/script.js
+++ scripts/script.js
@@ -12,155 +12,186 @@
 // will search for photo with this subject in title / description
 // oldpix date 1932
 // finds photo to match date in date field of LoC json (also notes if date is outside range of Depression-era FSA collection)
-// oldpix famous
-// randomly selects one of four famous photographers from Depression-era archive and serves up a photo of theirs
 
 
 module.exports = function(robot) {
 
-     robot.respond(/oldpix date (.*)/i, function(msg) {
-      var photoQuery;
-      var locURL = 'http://www.loc.gov/pictures/search/?fo=json&fa=displayed:anywhere&fi=date&q=';
-        photoQuery = escape(msg.match[1]);
-        return msg.http(locURL + photoQuery).get()(function(err, res, body) {
-          var image, images, photographer, response, date, fsaSuggest, photourl, title;
-          if (photoQuery > 1944 || photoQuery < 1935) {
-            let fsaSuggest = '\n (note: the depression years archive at LOC covers 1935-1944)';
-          } else { let fsaSuggest = ' ';
-          };
-          response = JSON.parse(body);
-          if (response.search.hits > 2) {
-              return msg.http('http://www.loc.gov/pictures/search/?fo=json&q=1820&fi=date&sp=2&fa=displayed%3Aanywhere').get()(function(err, res, body) {
-                var response2, title2;
-                response2 = JSON.parse(body);
-                title2 = response2.results[0].title;
-
-            let images = response.results;
-            let rando = getRando(0,19);
-            if (response.results[rando].title !== null) {
-              title = response.results[rando].title;
-            } else {title = 'no title'};
-
-            if (response.results[rando].created_published_date !== null) {
-              date = response.results[rando].created_published_date;
-            } else {date = 'no date'};
-
-            if (response.results[rando].image.full !== null) {
-              photourl = response.results[rando].image.full;
-            } else {photourl = 'no photo jpeg'};
-
-            if (response.results[rando].creator !== null) {
-              photographer = response.results[rando].creator;
-            } else {photographer = 'no photographer credit'};
-            if (images.length > 0) {
-            return msg.send( 'http:' + photourl + '\n find date: *' + photoQuery + '* \n photo date: ' + date + '   \n photo title: ' + title + '\n by: ' + fixName(photographer) + '\n more info at - http:' + response.results[rando].links.item);
-          }
-            });
-        }  else {
-          return msg.send('\n  not much turned up for: ' + photoQuery);}
-        });
-      });
-
-
-       robot.respond(/oldpix find (.*)/i, function(msg) {
-            var photoQuery;
-              photoQuery = escape(msg.match[1]);
-              return msg.http('http://www.loc.gov/pictures/search/?fo=json&fa=displayed:anywhere&q=' + photoQuery).get()(function(err, res, body) {
-                var image, images, photographer, response, date, photourl, title, hits;
-                response = JSON.parse(body);
-                if ( response.search.hits !== null || response.search.hits > 3 || response.results !== 'undefined' || response.results !== null) {
-                  images = response.results;
-                  let rando = getRando(0,19);
-                  if (response.results[rando].title !== 'undefined') {
-                    title = response.results[rando].title;
-                  } else {title = "no title"};
-
-                  if (response.results[rando].created_published_date !== "undefined") {
-                    date = response.results[rando].created_published_date;
-                  } else {date = "no date"};
-
-                  if (response.results[rando].image.full !== undefined) {
-                    photourl = response.results[rando].image.full;
-                  } else {photourl = "no photo jpeg "};
-
-                  if (response.results[rando].creator !== undefined) {
-                    photographer = response.results[rando].creator;
-                  } else {photographer = "no photographer credit"};
-                  if (images.length > 0) {
-                  return msg.send(  "http:" + photourl + "\n find: *" + photoQuery  + "* \n photo date: " + date + "   \n photo title: " + title + "\n by: " + fixName(photographer) + "\n more info at - http:" + response.results[rando].links.item );
-                }
-              }  else { return msg.send( photoQuery  + "- too few results for this term! try again, please." );}
-              });
-            });
-
-
-              const famousPhotographers = {
-                "photographers":[
-                  {"name": "Dorothea Lange",
-                   "search": "lange, dorothea"
-                  },
-                  {"name": "Russell Lee",
-                   "search": "lee, russell"
-                  },
-                  {"name": "Walker Evans",
-                   "search": "evans, walker"
-                  },
-                  {"name": "Marion Post Wolcott",
-                  "search": "wolcott, marion post"
-                  }
-                ]
-              };
-
-
-
-               robot.hear(/oldpix famous/i, function(msg) {
-                let rando = getRando(0,famousPhotographers.photographers.length);
-                let famousName =  famousPhotographers.photographers[rando].name;
-                let photoQuery = famousPhotographers.photographers[rando].search;
-                msg.http('http://www.loc.gov/pictures/search/?fo=json&fa=displayed:anywhere&fi=contributor&q=' + photoQuery).get()(function(err, res, body) {
-                  var image, images, response, photourl, title;
-                  response = JSON.parse(body);
-                  if (response.results !== undefined) {
-                    let images = response.results;
-                    let rando = getRando(0,19);
-                    if (response.results[rando].title !== undefined) {
-                      title = response.results[rando].title;
-                    } else {title = "no title"};
-
-                    if (response.results[rando].created_published_date !== undefined) {
-                      date = response.results[rando].created_published_date;
-                    } else {date = "no date"};
-
-                    if (response.results[rando].image.full !== undefined) {
-                      photourl = response.results[rando].image.full;
-                    } else {photourl = "no photo jpeg"};
-
-                    if (images.length > 0) {
-                    return msg.send( "http:" + photourl  + "\n Here's an old photo by a famous photographer named *" + famousName + "*   \n photo title: " + title  + "\n more info at - http:" + response.results[rando].links.item );
-                  }
-                  }  else { return msg.send( photoQuery  + "didn't turn anything up for some reason" );}
-                });
-              });
-
-
-
-    function getRando(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min)) + min;
-    };
-
-
-    function fixName (name) {
-      if (name === undefined || name === null) {
-        name = "no photographer credit"
+  robot.respond(/oldpix date (.*)/i, function(msg) {
+    var dateQuery;
+    var locURL = 'http://www.loc.gov/pictures/search/?fo=json&fa=displayed:anywhere&fi=date&q=';
+    dateQuery = escape(msg.match[1]);
+    return msg.http(locURL + dateQuery).get()(function(err, res, body) {
+      var image, photographer, response, date, fsaSuggest, photourl, title, pageList, whichResp;
+      if (dateQuery > 1944 || dateQuery < 1935) {
+        fsaSuggest = '\n (note: the depression years archive at LOC covers 1935-1944)';
       } else {
-        comma = name.includes(",");
-        if (comma === true) {
-          return name = name.split(',').reverse().join(" ");
-        }
-        else {return name;}
+        fsaSuggest = ' ';
+      };
+      response = JSON.parse(body);
+      var allResponses = [];
+      allResponses.push(response);
+      if (response.pages.total > 2) {
+        var response2;
+        return msg.http(locURL + dateQuery + '&sp=' + getRando(2, response.pages.total)).get()(function(err, res, body) {
+          response2 = JSON.parse(body);
+          allResponses.push(response2);
+          let whichResp = Math.floor(Math.random() * 2);
+          let rando = getRando(0, 19);
+
+          if (allResponses[whichResp].results[rando].title !== null) {
+            title = allResponses[whichResp].results[rando].title;
+          } else {
+            title = 'no title'
+          };
+
+          if (allResponses[whichResp].results[rando].created_published_date !== null) {
+            date = allResponses[whichResp].results[rando].created_published_date;
+          } else {
+            date = 'no date'
+          };
+
+          if (allResponses[whichResp].results[rando].image.full !== null) {
+            photourl = allResponses[whichResp].results[rando].image.full;
+          } else {
+            photourl = 'no photo jpeg'
+          };
+
+          if (allResponses[whichResp].results[rando].creator !== null) {
+            photographer = allResponses[whichResp].results[rando].creator;
+          } else {
+            photographer = 'no photographer credit'
+          };
+
+          return msg.send('http:' + photourl + '\n find date: *' + dateQuery + fsaSuggest + '* \n photo date: ' + date + '   \n photo title: ' + title + '\n by: ' + fixName(photographer) + '\n more info at - http:' + allResponses[whichResp].results[rando].links.item);
+
+        });
+      } else {
+        return msg.send('\n  not much turned up for: *' + dateQuery + '*');
       }
-    };
+    });
+  });
+
+
+  robot.respond(/oldpix find (.*)/i, function(msg) {
+    var photoQuery;
+    var locURL = 'http://www.loc.gov/pictures/search/?fo=json&fa=displayed:anywhere&q=';
+    photoQuery = escape(msg.match[1]);
+    return msg.http(locURL + photoQuery).get()(function(err, res, body) {
+      var image, images, photographer, response, date, photourl, title, hits;
+      response = JSON.parse(body);
+      var allResponses = [];
+      allResponses.push(response);
+
+      if (response.pages.total > 2) {
+        var response2;
+        return msg.http(locURL + photoQuery + '&sp=' + getRando(2, response.pages.total)).get()(function(err, res, body) {
+          response2 = JSON.parse(body);
+          allResponses.push(response2);
+          let whichResp = Math.floor(Math.random() * 2);
+          let rando = getRando(0, 19);
+
+          if (allResponses[whichResp].results[rando].title !== null) {
+            title = allResponses[whichResp].results[rando].title;
+          } else {
+            title = 'no title'
+          };
+
+          if (allResponses[whichResp].results[rando].created_published_date !== null) {
+            date = allResponses[whichResp].results[rando].created_published_date;
+          } else {
+            date = 'no date'
+          };
+
+          if (allResponses[whichResp].results[rando].image.full !== null) {
+            photourl = allResponses[whichResp].results[rando].image.full;
+          } else {
+            photourl = 'no photo jpeg'
+          };
+
+          if (allResponses[whichResp].results[rando].creator !== null) {
+            photographer = allResponses[whichResp].results[rando].creator;
+          } else {
+            photographer = 'no photographer credit'
+          };
+
+          return msg.send('http:' + photourl + '\n find: *' + photoQuery + '* \n photo date: ' + date + '   \n photo title: ' + title + '\n by: ' + fixName(photographer) + '\n more info at - http:' + allResponses[whichResp].results[rando].links.item);
+
+        });
+      } else {
+        return msg.send('\n  not much turned up for: *' + photoQuery + '*');
+      }
+    });
+  });
+
+  //
+  // const famousPhotographers = {
+  //   "photographers":[
+  //     {"name": "Dorothea Lange",
+  //      "search": "lange, dorothea"
+  //     },
+  //     {"name": "Russell Lee",
+  //      "search": "lee, russell"
+  //     },
+  //     {"name": "Walker Evans",
+  //      "search": "evans, walker"
+  //     },
+  //     {"name": "Marion Post Wolcott",
+  //     "search": "wolcott, marion post"
+  //     }
+  //   ]
+  // };
+  //
+  //
+  //
+  //  robot.hear(/oldpix famous/i, function(msg) {
+  //   let rando = getRando(0,famousPhotographers.photographers.length);
+  //   let famousName =  famousPhotographers.photographers[rando].name;
+  //   let photoQuery = famousPhotographers.photographers[rando].search;
+  //   msg.http('http://www.loc.gov/pictures/search/?fo=json&fa=displayed:anywhere&fi=contributor&q=' + photoQuery).get()(function(err, res, body) {
+  //     var image, images, response, photourl, title;
+  //     response = JSON.parse(body);
+  //     if (response.results !== undefined) {
+  //       let images = response.results;
+  //       let rando = getRando(0,19);
+  //       if (response.results[rando].title !== undefined) {
+  //         title = response.results[rando].title;
+  //       } else {title = "no title"};
+  //
+  //       if (response.results[rando].created_published_date !== undefined) {
+  //         date = response.results[rando].created_published_date;
+  //       } else {date = "no date"};
+  //
+  //       if (response.results[rando].image.full !== undefined) {
+  //         photourl = response.results[rando].image.full;
+  //       } else {photourl = "no photo jpeg"};
+  //
+  //       if (images.length > 0) {
+  //       return msg.send( "http:" + photourl  + "\n Here's an old photo by a famous photographer named *" + famousName + "*   \n photo title: " + title  + "\n more info at - http:" + response.results[rando].links.item );
+  //     }
+  //     }  else { return msg.send( photoQuery  + "didn't turn anything up for some reason" );}
+  //   });
+  // });
+
+
+
+  function getRando(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+
+  function fixName(name) {
+    if (name === undefined || name === null) {
+      name = "no photographer credit"
+    } else {
+      comma = name.includes(",");
+      if (comma === true) {
+        return name = name.split(',').reverse().join(" ");
+      } else {
+        return name;
+      }
+    }
+  };
 
 };
